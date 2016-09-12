ea03cb88c3fd764de2085555f690722409677812
joostlawerman <joost.lawerman@gmail.com>
2016-09-12 22:26:18 +0200

    Build 1.1.1

--- src/pages/components/Day.vue
+++ src/pages/components/Day.vue
@@ -36,10 +36,10 @@
 <template>
 	<div class="list">
 		<event
-			v-for="(index, event) in model.events"
+			v-for="(index, event) in events"
 			track-by="$index"
-			:first="index == 0"
-			:last="index == model.events.length - 1"
+			:first="index === 0"
+			:last="index == events.length - 1"
 			:event="event">
 
 		</event>
@@ -47,6 +47,8 @@
 </template>
 <script>
 	import Event from './Event'
+
+	import moment from 'moment';
 
 	// NOTE: https://material.google.com/components/subheaders.html#subheaders-list-subheaders
 
@@ -59,13 +61,95 @@
 		},
 		data() {
 			return {
-
+				colors: [
+					'4CAF50',
+					'FF9800',
+					'2196F3',
+					'3F51B5',
+					'FFEB3B'
+				],
+				events: []
 			}
 		},
 		created() {
-			// props.events.reduce((event, events) => {
-			//
-			// })
+			var events = [], loops = 0;
+			for (event in this.model.events) {
+
+				if (event in this.model.events) {
+					loops++;
+
+					event = this.model.events[event]
+
+					event.start_stamp = this.parseTime(event.start);
+					event.end_stamp = this.parseTime(event.end)
+
+					if (events.length   > 0) {
+						const last = events[events.length - 1];
+
+						if (!event.end_stamp.isSame(last.start_stamp)) {
+							events.push({
+								contents: 'Break',
+								start: last.end,
+								end: event.start,
+								end_stamp: event.start_stamp,
+								color: "9E9E9E",
+								icon: "access_time"
+							});
+						}
+					}
+
+					var icons = [
+						'local_activity',
+						'import_contacts',
+						'layers',
+						'photo_camera'
+					];
+
+					event.icon = icons[Math.round(Math.random() * (icons.length - 1))];
+
+					event.color = this.getColor();
+
+					events.push(event)
+
+					if (loops == this.model.events.length) {
+						events.push({
+							contents: 'End of ' + event.start_stamp.format('dddd'),
+							start: event.end_stamp.format('hh:mm'),
+							end: event.end,
+							end_stamp: event.end_stamp,
+							color: "9E9E9E",
+							icon: "looks"
+						});
+					}
+				}
+			}
+
+			this.events = events;
+		},
+		methods: {
+			parseTime(string) {
+				const time = string.split(':')
+
+ 				return moment(this.model.timestamp * 1000).subtract(1, 'd').add({
+					hours: time[0],
+					minutes: time[1]
+				});
+			},
+			getColor() {
+				if (this.colors.length > 0) {
+					return this.colors.shift()
+				}
+
+				this.colors = [
+					'4CAF50',
+					'FF9800',
+					'2196F3',
+					'3F51B5',
+					'FFEB3B'
+				];
+
+				return this.getColor();
+			}
 		}
 	}
 
--- src/pages/components/Event.vue
+++ src/pages/components/Event.vue
@@ -60,14 +60,14 @@
 <template>
 	<div class="list-item">
 		<div class="icon">
-			<i class="material-icons md-18" :style="'color:' + color">{{ icon }}</i>
-			<div v-if="!first" class="bar-top" :style="'background-color:' + color"></div>
-			<div v-if="!last" class="bar-bottom" :style="'background-color:' + color"></div>
+			<i class="material-icons md-18" :style="'color:#' + event.color">{{ event.icon }}</i>
+			<div v-if="!first" class="bar-top" :style="'background-color:#' + event.color"></div>
+			<div v-if="!last" class="bar-bottom" :style="'background-color:#' + event.color"></div>
 		</div>
 		<div class="info">
 			<span class="start">{{ event.start }}</span>
 			<br>
-			<span class="contents">{{ event.contents }}</span>
+			<span class="contents">{{{ event.contents }}}</span>
 		</div>
 		<span class="location">{{ event.location }}</span>
 	</div>
@@ -83,39 +83,14 @@
 		},
 		data() {
 			return {
-				color: '#6200EA',
 				icon: 'import_contacts',
 				time: {}
 			}
 		},
 		created() {
-			var colors = [
-				'4CAF50',
-				'FF9800',
-				'2196F3',
-				'3F51B5',
-				'FFEB3B'
-			]
-
-			var icons = [
-				'local_activity',
-				'import_contacts',
-				'layers',
-				'photo_camera'
-			]
-
-			this.color = '#'+colors[Math.round(Math.random() * (colors.length - 1))];
-			this.icon = icons[Math.round(Math.random() * (icons.length - 1))];
-
-			const time = this.event.end.split(':')
-
-			this.time = moment(this.$parent.model.timestamp * 1000).subtract(1, 'd').add({
-				hours: time[0],
-				minutes: time[1]
-			})
-
-			if (this.time.isBefore(moment())) {
-				this.color = '#9E9E9E'
+			console.log(this.event.color)
+			if (this.event.end_stamp.isBefore(moment())) {
+				this.event.color = '9E9E9E'
 			}
 		}
 	}
